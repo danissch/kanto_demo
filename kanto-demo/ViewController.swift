4d9226a68cd2e1834bd49747423c87229367682e
Daniel Schutz <danissch@gmail.com>
2020-10-30 14:31:17 -0500

    Initial Commit

---
+++ kanto-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kanto-demo
+//
+//  Created by Daniel Durán Schutz on 30/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
